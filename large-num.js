a9cb96b95d33e338d631c48a24b95356945500d0
swaroopatarkase <tarkaseswaroopa@gmail.com>
2024-07-12 22:05:36 +0530

    code added

--- large-num.js
+++ large-num.js
@@ -1,10 +1,10 @@
-const number =[67,89,76,145]
+const number =[67,89,76,45]
 let largest = number[0]
-for(num of number)
+for(const num of number)
 {
     if(num>largest)
     {
         largest=num
     }
 }
-console.log(`Largest Number is: ${num}`)
+console.log(`Largest Number is: ${largest}`)
